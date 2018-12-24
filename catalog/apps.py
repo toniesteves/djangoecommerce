34ae8e82686ff8a4de0d796b9e4e4272fdbd3aa7
Toni Esteves <toni.esteves@gmail.com>
2018-12-24 16:16:48 -0300

    customize django admin

--- catalog/apps.py
+++ catalog/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
-    name = 'catalog'
+  name = 'catalog'
+  verbose_name = 'Catálogo'
--- catalog/models.py
+++ catalog/models.py
@@ -8,43 +8,43 @@ from django.urls import reverse
 
 class Category(models.Model):
 
-    name = models.CharField('Name', max_length=100)
-    slug = models.SlugField('Slug', max_length=100)
+  name = models.CharField('Name', max_length=100)
+  slug = models.SlugField('Slug', max_length=100)
 
-    created = models.DateTimeField('Created_at', auto_now_add=True)
-    modified = models.DateTimeField('Updated_at', auto_now=True)
+  created = models.DateTimeField('Created', auto_now_add=True)
+  modified = models.DateTimeField('Modified', auto_now=True)
 
-    class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-        ordering = ['name']
+  class Meta:
+      verbose_name = 'Category'
+      verbose_name_plural = 'Categories'
+      ordering = ['name']
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
-    def get_absolute_url(self):
-        return reverse('catalog:category', kwargs={'slug': self.slug})
+  def get_absolute_url(self):
+      return reverse('catalog:category', kwargs={'slug': self.slug})
 
 
 class Product(models.Model):
 
-    name = models.CharField('Name', max_length=100)
-    slug = models.SlugField('Slug', max_length=100)
-    category = models.ForeignKey('catalog.Category', verbose_name='Category', on_delete=models.PROTECT)
-    description = models.TextField('Description', blank=True)
-    price = models.DecimalField('Price', decimal_places=2, max_digits=8)
+  name = models.CharField('Name', max_length=100)
+  slug = models.SlugField('Slug', max_length=100)
+  category = models.ForeignKey('catalog.Category', verbose_name='Category', on_delete=models.PROTECT)
+  description = models.TextField('Description', blank=True)
+  price = models.DecimalField('Price', decimal_places=2, max_digits=8)
 
 
-    created = models.DateTimeField('Created_at', auto_now_add=True)
-    modified = models.DateTimeField('Updated_at', auto_now=True)
+  created = models.DateTimeField('Created', auto_now_add=True)
+  modified = models.DateTimeField('Modified', auto_now=True)
 
-    class Meta:
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
-        ordering = ['name']
+  class Meta:
+      verbose_name = 'Product'
+      verbose_name_plural = 'Products'
+      ordering = ['name']
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
-    def get_absolute_url(self):
-        return reverse('catalog:product', kwargs={'slug': self.slug})
+  def get_absolute_url(self):
+      return reverse('catalog:product', kwargs={'slug': self.slug})
