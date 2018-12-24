fa953976e51b05ca8cad305cc378bf21eeb68187
Toni Esteves <toni.esteves@gmail.com>
2018-12-24 17:24:42 -0300

    filtering products by category

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', views.products, name='products'),
+    re_path(r'^(?P<slug>[\w_-]+)/', views.category, name='category')
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from catalog.models import Product
+from catalog.models import Product, Category
 
 def product(request):
   return render(request, 'product.html')
 
 def products(request):
   context = {
-  'products': Product.objects.all()
+    'products': Product.objects.all()
   }
   return render(request, 'catalog/products.html', context)
+
+def category(request, slug):
+  category = Category.objects.get(slug=slug)
+  context = {
+    'current_category': category,
+    'products': Product.objects.filter(category=category)
+  }
+  return render(request, 'catalog/category.html', context)
